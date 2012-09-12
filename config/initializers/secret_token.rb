fa6600dcc10916cc020048a1f73b266d5fb5d125
Stacy Vlasits <stacyvlasits@gmail.com>
2012-09-12 09:28:11 -0500

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ocill::Application.config.secret_token = '3849ff56f92cc9b2f4de916f8ca221d23dcf68c976545b3c3f1649228e4c9de6c417726ab475dee4637338b848c43c4b603b6daa75580e98adaf14ea8428f933'
