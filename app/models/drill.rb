74bbe76d5326a858cdb874a9e1a1b89fe2e4f16f
Stacy Vlasits <stacyvlasits@gmail.com>
2012-10-23 16:32:42 -0500

    added a relation between drill and template models

--- app/models/drill.rb
+++ app/models/drill.rb
@@ -1,5 +1,6 @@
 class Drill < ActiveRecord::Base
   attr_accessible :instructions, :order, :prompt, :responses, :title
   belongs_to :lesson
+  belongs_to :template
   has_many :exercises
 end
--- app/models/template.rb
+++ app/models/template.rb
@@ -1,3 +1,4 @@
 class Template < ActiveRecord::Base
   attr_accessible :desc, :name
+  has_many :drills
 end
