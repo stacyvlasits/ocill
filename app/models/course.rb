fcc90826dd89566d882c04a87544d1e027516b6a
Stacy Vlasits <stacyvlasits@gmail.com>
2012-11-01 08:16:39 -0500

    modified the schema to add attributes for model children and grandchildren

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,16 @@
 class Course < ActiveRecord::Base
   attr_accessible :position, :title, :lessons_attributes
   has_many :lessons
+  has_many :drills, :through => :lessons
   accepts_nested_attributes_for :lessons, allow_destroy: true
+
+  after_initialize :set_default_position
+
+  def set_default_position
+    self.position ||= 1
+  end
+
+  # def drills
+  #   self.lessons.collect {|l| l.drills}
+  # end
 end
--- app/models/exercise_item.rb
+++ app/models/exercise_item.rb
@@ -1,5 +1,5 @@
 class ExerciseItem < ActiveRecord::Base
-  attr_accessible :scored, :text, :type, :media_items_attributes
+  attr_accessible :graded, :exercise_item_type, :column, :answer, :text, :type, :media_items_attributes
   belongs_to :exercise
   has_many :media_items, :dependent => :destroy
   accepts_nested_attributes_for :media_items, allow_destroy: true
