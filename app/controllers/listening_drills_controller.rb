77f5299e9156d1eb8ca58b5e0f168c8b055948af
Stacy Vlasits <stacyvlasits@gmail.com>
2012-11-16 10:30:40 -0600

    fixed drills up and removed active admin

---
+++ app/controllers/listening_drills_controller.rb
@@ -0,0 +1,24 @@
+class ListeningDrillsController < ApplicationController
+  def index
+    @drills = ListeningDrill.all
+  end
+
+  def new
+    @drill = ListeningDrill.new
+  end
+
+  def edit
+    @drill = ListeningDrill.find(params[:id])
+  end
+
+  def create
+    @drill = ListeningDrill.new(params[:listening_drill])
+    if @drill.save
+      redirect_to @drill
+    else
+      render :action => "new"
+    end
+  end
+
+
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,28 @@
 module ApplicationHelper
-  def link_to_add_fields(name, f, association, path)
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.send(association).klass.new
+    id = new_object.object_id
+    fields = f.fields_for(association, new_object, child_index: id) do |builder|
+      render(association.to_s.singularize + "_fields", f: builder)
+    end
+    link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+  end
+
+  def header_row(column_names, options = {})
+    html = "<tr>"
+    column_names.each do |k, v| 
+      html += '<th scope="col">' 
+      html += options[:f].text_field :column_names, :class => 'text_area' if options[:editable]
+      html += (v || "&nbsp;") + '</th>' unless options[:editable]
+    end
+    html += '</tr>'
+  end
+#delete this
+  def path_aware_link_to_add_fields(name, f, association, path)
     new_object = f.object.send(association).klass.new
     id = new_object.object_id
     split_path = path.split('/')
-    
     prefix = split_path[-2] == "views" ?  "" : split_path.last(2).join('/') + "/"
-    # binding.pry unless prefix.size > 1
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
       render(prefix + association.to_s.singularize + "_fields", f: builder)
     end
