02a7a26327e1f262fbab8a73b9514b371bf22c9f
Stacy Vlasits <stacyvlasits@gmail.com>
2013-01-31 13:53:36 -0600

    removed unit tests that are outside of rspec

--- app/models/delete/template.rb
+++
@@ -1,4 +0,0 @@
-class Template < ActiveRecord::Base
-  attr_accessible :desc, :name
-  has_many :drills
-end
--- test/unit/helpers/exercise_item_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ExerciseItemHelperTest < ActionView::TestCase
-end
