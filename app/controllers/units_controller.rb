d1cce69102facf0137448b5ce08b13e2a0b4a8f7
Stacy Vlasits <stacyvlasits@gmail.com>
2013-06-06 12:07:53 -0500

    fixed bugs in units controller

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -1,14 +1,23 @@
 class UnitsController < InheritedResources::Base
   respond_to :json
   def create
-    super do |format|
-      format.html { redirect_to units_url }
+    @unit = Unit.new(params[:unit])
+    if @unit.save
+      flash[:notice] = "Successfully created unit."
+      redirect_to units_url
+    else
+
+      render :action => 'new'
     end
   end
 
   def update
-    super do |format|
-      format.html { redirect_to units_url }
+    @unit = Unit.find(params[:id])
+    if @unit.update_attributes(params[:unit])
+      flash[:notice] = "Successfully updated unit."
+      redirect_to units_url
+    else
+      respond_with(@unit)
     end
   end
 end
