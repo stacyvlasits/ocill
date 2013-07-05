56caae191f226defde5dce9f9765a32dae8b25b6
Stacy Vlasits <stacyvlasits@gmail.com>
2013-07-05 16:08:49 -0500

    fixing nested attempts resources

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -1,8 +1,8 @@
 class AttemptsController < InheritedResources::Base
-
   def new
     if params[:drill_id]
       @attempt = Attempt.create(:drill_id => params[:drill_id])
+      @drill = @attempt.drill
       new_responses = Drill.find(params[:drill_id]).exercise_items.count || 1
     else
       not_found 'There is no drill here for you attempt'
