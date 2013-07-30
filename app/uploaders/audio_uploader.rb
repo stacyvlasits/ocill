cf1f5ec9db8c56cf5d589918136ef91051841f66
Stacy Vlasits <stacyvlasits@gmail.com>
2013-07-30 09:44:21 -0500

    included course title in path for audio

--- app/uploaders/audio_uploader.rb
+++ app/uploaders/audio_uploader.rb
@@ -20,7 +20,8 @@ class AudioUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    course_title = model.drill.course.title.parameterize if model.drill.course
+    "uploads/#{course_title}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
