4a012eb1304a72ec11a130d71d348eda282666aa
Stacy Vlasits <stacyvlasits@gmail.com>
2013-08-21 12:52:17 -0500

    fix tinymce custom stylsheet bug

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -20,13 +20,15 @@ class Ability
       can :manage, Drill do |drill|
         drill.course.roles.is_instructor_or_admin(user).count > 0
       end
+      can :create, Drill      
       can :manage, Exercise do |exercise|
         exercise.drill.course.roles.is_instructor_or_admin(user).count > 0
       end
+      can :create, Exercise      
       can :manage, ExerciseItem do |exercise_item|
         exercise_item.drill.course.roles.is_instructor_or_admin(user).count > 0
       end
-      can :create, Drill
+      can :create, ExerciseItem      
       can :manage, Attempt  # for debugging purposes only.
       can :read, Course
       can :read, Unit
