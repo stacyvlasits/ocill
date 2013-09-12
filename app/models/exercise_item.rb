b4461fd99cb4096b003aa81ea7c6bbcf9ee756c1
Stacy Vlasits <stacyvlasits@gmail.com>
2013-09-12 15:53:18 -0500

    added panda support to exercise items

--- app/models/exercise_item.rb
+++ app/models/exercise_item.rb
@@ -1,10 +1,12 @@
 class ExerciseItem < ActiveRecord::Base
-  attr_accessible :graded, :header_id, :exercise_item_type, :acceptable_answers, :text, :type, :image, :audio, :video
+  attr_accessible :graded, :header_id, :exercise_item_type, :acceptable_answers, :text, :type, :image, :audio, :video, :panda_audio_id
+  # attr_accessible  :encodings  
   attr_accessible :position, :remove_audio, :remove_image, :remove_video # TODO remove "column" from db
   mount_uploader :audio, AudioUploader
 #  mount_uploader :video, VideoUploader
   mount_uploader :image, ImageUploader
   serialize :acceptable_answers
+  serialize :encodings, Hash
 
   has_many :responses
 
@@ -15,6 +17,26 @@ class ExerciseItem < ActiveRecord::Base
   after_initialize :set_default_position
   before_save :cleanup_audio
   
+  def panda_audio
+    @panda_audio ||= Panda::Video.find(panda_audio_id)
+  end
+
+  def self.serialized_attr_accessor(*args)
+    args.each do |method_name|
+      eval "
+        def #{method_name}
+          (self.encodings || {})[:#{method_name}]
+        end
+        def #{method_name}=(value)
+          self.encodings ||= {}
+          self.encodings[:#{method_name}] = value
+        end
+        attr_accessible :#{method_name}
+      "
+    end
+  end  
+
+  serialized_attr_accessor :mp3, :ogg  
   
   def cleanup_audio
     if remove_audio
