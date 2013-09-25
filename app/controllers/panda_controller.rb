f069d8109842b2e58191c485a9e96b0be4e3833f
Stacy Vlasits <stacyvlasits@gmail.com>
2013-09-25 14:27:39 -0500

    tried to fix notification controller

--- app/controllers/panda_controller.rb
+++ app/controllers/panda_controller.rb
@@ -3,34 +3,27 @@ class PandaController < ApplicationController
 
   def notifications
     if params[:event] == 'video-encoded'
-        model = Exercise.find_by_panda_audio_id(params[:video_id])
-        model ||= ExerciseItem.find_by_panda_audio_id(params[:video_id]) 
-        
-        if model
-          if model.panda_audio.status == 'fail'
-            #  TODO insert error handling?
-          else
-            mp3 = model.panda_audio.encodings['mp3']
-            ogg = model.panda_audio.encodings['ogg']
+      model = Exercise.find_by_panda_audio_id(params[:video_id])
+      model ||= ExerciseItem.find_by_panda_audio_id(params[:video_id]) 
+      
+      if model
+        if model.panda_audio.status == 'success'
 
-            if mp3.status == 'success'
-              model.mp3 = mp3.url
+          mp3 = model.panda_audio.encodings['mp3']
+          ogg = model.panda_audio.encodings['ogg']
 
-            else
-             # model.mp3_errors = "Something went wrong while uploading please email the url of this page to #{ENV["SUPPORT_EMAIL"]}"
-            end
-
-            if ogg.status == 'success'
-                model.ogg = ogg.url
-            else 
-             # model.ogg_errors = "Something went wrong while uploading please email the url of this page to #{ENV["SUPPORT_EMAIL"]}"
-            end
+          if mp3.status == 'success'
+            model.mp3 = mp3.url
           end
-          model.save!
 
-        else
-            #  TODO insert error handling?
+          if ogg.status == 'success'
+              model.ogg = ogg.url
+          else 
+           # model.ogg_errors = "Something went wrong while uploading please email the url of this page to #{ENV["SUPPORT_EMAIL"]}"
+          end
+        model.save!
         end
+      end
     end
     render nothing: true
   end
