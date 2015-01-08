46563b32ec6e526a39b8dbeafc7d22109a1c823b
Stacy Vlasits <stacyvlasits@gmail.com>
2015-01-08 08:28:47 -0600

    destroy dependent activities when section is destroyed

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,6 +1,6 @@
 class Section < ActiveRecord::Base
   attr_accessible :lti_course_id
-  has_many :activities
+  has_many :activities, :dependent => :destroy
   # TODO:  User or remove course_id
 end
   
