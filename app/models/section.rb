4b3e853ef1f84020e0dec8a27fa31cc26025a6d2
Stacy Vlasits <stacyvlasits@gmail.com>
2015-01-14 13:54:50 -0600

    new build activities from parents #1

--- app/models/section.rb
+++ app/models/section.rb
@@ -4,15 +4,19 @@ class Section < ActiveRecord::Base
   # TODO:  use or remove course_id
   
   def build_activities_from_parent(parent, referrer_course_id)
-    # get all canvas assignments
     canvas = Canvas::API.new(:host => "https://utexas.instructure.com", :token => ENV["UT_CANVAS_TOKEN"])
     
+    # get all canvas assignments from the new course
     child_assignments = canvas.get("/api/v1/courses/#{referrer_course_id}/assignments", {'per_page' => '1000'})
     
     while child_assignments.more?
       child_assignments.next_page!
     end
 
+    # create a hash of the assignments
+    #  - filter out non-ocill assignments
+    #  - Key:   Assignment name
+    #  - Value: Resource Link Id
     ocill_child_assignments = {}
 
     child_assignments.each do |assignment| 
@@ -22,10 +26,31 @@ class Section < ActiveRecord::Base
       end
     end
 
+    # get all canvas assignments from the old course
+    parent_assignments = canvas.get("/api/v1/courses/#{parent.canvas_course_id}/assignments", {'per_page' => '1000'})
+
+    while parent_assignments.more?
+      parent_assignments.next_page!
+    end
+
+    # create a hash of the assignments
+    #  - filter out non-ocill assignments
+    #  - Key:   Assignment name
+    #  - Value: Resource Link Id
+    ocill_parent_assignments = {}
+
+    parent_assignments.each do |assignment| 
+      if assignment["submission_types"].include?("external_tool") && (assignment["external_tool_tag_attributes"]["url"] == "https://ocill.herokuapp.com/launch/create") 
+
+        ocill_parent_assignments[assignment["name"]] = assignment["external_tool_tag_attributes"]["resource_link_id"]
+      end
+    end
+
     parent.activities.each do | activity |
-      drill = activity.drill
-      if drill
-        resource_link_id = ocill_child_assignments[drill.title]
+      if activity.drill_id
+        drill_name = ocill_parent_assignments.key(activity.lti_resource_link_id)
+
+        resource_link_id = ocill_child_assignments[drill_name]
 
         self.activities.create(
           :lti_resource_link_id  =>  resource_link_id,
