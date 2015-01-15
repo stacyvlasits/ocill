67bb3e862c1e5e93d9c04b71a042ffe70f6a02e0
Stacy Vlasits <stacyvlasits@gmail.com>
2015-01-15 16:06:09 -0600

    add canvas course duplication page #9

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -3,14 +3,14 @@ class SectionsController < ApplicationController
   respond_to :html
 
   def new
-    parent_section_id        = params[:parent_section_id]
-    canvas_course_id        = params[:canvas_course_id]
-    lti_course_id           = params[:lti_course_id]
-    parent_canvas_course_id = params[:parent_canvas_course_id]
+    @parent_section_id       = params[:parent_section_id]
+    @canvas_course_id        = params[:canvas_course_id]
+    @lti_course_id           = params[:lti_course_id]
+    @parent_canvas_course_id = params[:parent_canvas_course_id]
     
     @section = Section.new
 
-    respond_with(@section, parent_section_id, canvas_course_id, lti_course_id, parent_canvas_course_id)
+    respond_with(@section, @parent_section_id, @canvas_course_id, @lti_course_id, @parent_canvas_course_id)
   end
 
   def create
