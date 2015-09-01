2ed234affb7a7c15075b7a321b60a282e05f6e66
Stacy Vlasits <stacyvlasits@gmail.com>
2015-09-01 14:33:19 -0500

    Revert "Revert "fixing problem with safari and other browsers that hate third party cookies""
    This reverts commit 48774a48119ff214d760b359fb776573368c5a37.

--- app/models/launch.rb
+++ app/models/launch.rb
@@ -1,6 +1,6 @@
 class Launch
   require 'oauth/request_proxy/rack_request'
-  attr_reader :params, :errors, :tool, :user, :activity, :section, :session, :parent_section, :duplicate_session_data
+  attr_reader :params, :errors, :tool, :user, :activity, :section, :session, :parent_section, :duplicate_session_data, :request
 	
   def initialize(request, params)
     @params = params['launch_params'] || params
@@ -12,6 +12,7 @@ class Launch
     @user = find_user
     @section = find_section
     @activity = find_activity
+
   end
 
   def to_be_duplicated?
@@ -49,12 +50,12 @@ class Launch
   end
 
   def lti_roles_to_ocill_user_role(lti_roles)
-    roles = lti_roles.split(',') if lti_roles 
-    if roles.include?("Instructor")
+    roles = lti_roles.split(',') if lti_roles
+    if roles.grep(/Instructor/).any?
       "Instructor"
     elsif roles.grep(/TeachingAssistant/).any?
       "Instructor"  
-    elsif roles.include?("Learner") || roles.include?("learner")
+    elsif roles.grep(/[lL]earner/).any?
       "Learner"
     else
       error_message = "The Launch model just created a new user as a Learner, even though the user was not properly identified"
@@ -98,10 +99,12 @@ class Launch
   end
 
   def find_user
-    role = lti_roles_to_ocill_user_role(params[:roles])
+    user_id = params[:user_id] || params["user_id"]
+    roles = params[:roles] || params["ext_roles"]
+    role = lti_roles_to_ocill_user_role(roles)
     email = "user#{rand(10000..999999999999).to_s}@example.com"
     password = "pass#{rand(10000..999999999999).to_s}"
-    u = User.find_or_create_by_lti_user_id(lti_user_id: params[:user_id], role: role, email: email, password: password)
+    u = User.find_or_create_by_lti_user_id(lti_user_id: user_id, role: role, email: email, password: password)
   end
 
   def find_or_create_child_section(context_id, custom_canvas_course_id)
@@ -152,10 +155,15 @@ class Launch
   end
 
   def canvas_course_id
+
     referrer_uri = URI.parse(@request.referrer)
     path_parts = referrer_uri.path.split("/")
-    course_id_index = path_parts.find_index("courses") + 1
-    course_id = path_parts[course_id_index]
+    course_id_index = path_parts.find_index("courses")
+    if course_id_index
+      course_id = path_parts[course_id_index + 1]
+    else 
+      course_id = @request.params["course_id"]
+    end
   end
   
 private
