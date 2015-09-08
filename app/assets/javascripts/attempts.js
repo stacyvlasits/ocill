9814de7826261ec0bc9215ab6586fac675c2dde8
Stacy Vlasits <stacyvlasits@gmail.com>
2015-09-08 16:34:45 -0500

    fixed problem with drills scrolling

--- app/assets/javascripts/attempts.js
+++ app/assets/javascripts/attempts.js
@@ -1,35 +1,44 @@
 function moveScroller() {
     var move = function() {
-      var win = $(window);
-        var st = win.scrollTop();
-        var sa = $("#scroller-anchor")
-        var ot = sa.offset().top;
-        var s = $("#scroller");
-        if ( s.height() > ( win.height() / 2) ) {
-          s.css({ 
-            "height": win.height()/2,
-            "overflow": "scroll"
-          });
-        }
-        if(st > ot) {
-            s.css({
-                "position": "fixed",
-                "top": "0px",
-                "z-index": 5
-            });
-            sa.css({"height": s.css("height")});
-        } else {
-            if(st <= ot) {
-                s.css({
-                    "position": "relative",
-                    "top": "",
-                    "z-index": 1
-                });
-            sa.css({"height": 0 });
+            var doc = $(document);
+        if (doc.height() > 1800) {
+
+            var st = doc.scrollTop();
+            var sa = $("#scroller-anchor")
+            var ot = sa.offset().top;
+            var s = $("#scroller");
+            if ( s.height() > ( doc.height() / 2) ) {
+              s.css({ 
+                "height": doc.height()/2,
+                "overflow": "scroll"
+              });
             }
+            if(st > ot) {
+                s.css({
+                    "position": "fixed",
+                    "top": "0px",
+                    "z-index": 5
+                });
+                sa.css({"height": s.css("height")});
+            } else {
+                if(st <= ot) {
+                    s.css({
+                        "position": "relative",
+                        "top": "",
+                        "z-index": 1
+                    });
+                sa.css({"height": 0 });
+                }
+            }
+            console.log({
+                "st": st,
+                "ot": ot,
+                "doc height": doc.height(),
+                "sa": sa.css("height"),
+            });
         }
     };
-    $(window).scroll(move);
+    $(document).scroll(move);
     move();
 }
 
