30201a230eae0ac6c478a4b627aae1f8484eda54
Stacy Vlasits <stacyvlasits@gmail.com>
2016-03-04 10:47:01 -0600

    updated Carrierwave for Rails 4.0

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -7,10 +7,10 @@ class VideoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-  include Sprockets::Helpers::RailsHelper
-  include Sprockets::Helpers::IsolatedHelper
 
-  # Choose what kind of storage to use for this uploader:
+  include Sprockets::Rails::Helper
+
+    # Choose what kind of storage to use for this uploader:
   # storage :file
   storage :fog
 
