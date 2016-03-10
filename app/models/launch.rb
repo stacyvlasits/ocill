7059706cb5bbfb5ba1311b54599ff65ed9ece4f3
Stacy Vlasits <stacyvlasits@gmail.com>
2016-03-10 10:59:40 -0600

    testing

--- app/models/launch.rb
+++ app/models/launch.rb
@@ -22,19 +22,19 @@ class Launch
   end
 
   def authorize!
-    logger.info "**LAUNCH#autthorize!** [starting] "
+    Rails.logger.debug "**LAUNCH#autthorize!** [starting] "
      if key = @params['oauth_consumer_key']
       if secret = oauth_shared_secrets[key]
         @tool = Rails.cache.fetch(session[:launch_tool_cache_key], expires_in: 12.hours) do
           IMS::LTI::ToolProvider.new(key, secret, @params)
         end
-        logger.info "**LAUNCH#autthorize!** [tool built]  #{@tool.to_yaml} "
+        Rails.logger.debug "**LAUNCH#autthorize!** [tool built]  #{@tool.to_yaml} "
       else
         @tool = session[:launch_tool_cache_key] = IMS::LTI::ToolProvider.new(nil, nil, @params)
         @tool.lti_msg = "The consumer didn't use a recognized key."
         @tool.lti_errorlog = "You did it wrong!"
         @errors << "Consumer key wasn't recognized"
-        logger.info "**LAUNCH#autthorize!** [tool from session]  #{@tool.to_yaml} "
+        Rails.logger.debug "**LAUNCH#autthorize!** [tool from session]  #{@tool.to_yaml} "
         return self
       end
     else
@@ -44,12 +44,12 @@ class Launch
 
     if !@tool.valid_request?(@request)
       @errors << "The OAuth signature was invalid"
-      logger.info "**LAUNCH#autthorize!** [Signature invalid]"
+      Rails.logger.debug "**LAUNCH#autthorize!** [Signature invalid]"
       return self
     end
     unless Rails.env == "development"
       if Time.now.utc.to_i - @tool.request_oauth_timestamp.to_i > 4.hours
-        logger.info "**LAUNCH#autthorize!** [request too old]"
+        Rails.logger.debug "**LAUNCH#autthorize!** [request too old]"
         @errors << "Your request is too old."
         return self
       end
@@ -60,6 +60,8 @@ class Launch
   def lti_roles_to_ocill_user_role(lti_roles)
     roles = lti_roles.split(',') if lti_roles
     if roles.grep(/Instructor/).any?
+      "Instructor"
+    elsif roles.grep(/Administrator/).any?
       "Instructor"
     elsif roles.grep(/TeachingAssistant/).any?
       "Instructor"  
